6f1f85eb1c7cc539eab0474b8e71a286a2f5345c
Brent Caballero <brentcaballero5@gmail.com>
2022-02-06 19:53:40 -0600

    add div for displaying results under the game buttons

--- game.js
+++ game.js
@@ -4,6 +4,16 @@ let playerScore = 0;
 let computerScore = 0;
 
 function game() {
+    const scoreInfo = document.querySelector(".score-info");
+    const scoreMessage = document.createElement('p');
+    scoreInfo.appendChild(scoreMessage);
+
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playRound(playerSelection = `${button.id}`, computerSelection);
+        });
+    });
 
     function computerPlay() {
         guess = Math.floor(Math.random() * 3);
@@ -16,41 +26,32 @@ function game() {
             return "Scissors";
         }
     }
-
+    
     function playRound(playerSelection, computerSelection) {
         computerSelection = computerPlay().toLowerCase();
 
         if (computerSelection == playerSelection) {
             // results.textContent = "It's a tie!";
             console.log("It's a tie!");
+            scoreMessage.textContent = "It's a tie!";
         } else if (
             (computerSelection == "rock" && playerSelection == "scissors") || 
             (computerSelection == "paper" && playerSelection == "rock") ||
             (computerSelection == "scissors" && playerSelection == "paper")
         ) {
             computerScore++; 
-            /*results.textContent = `Computer's ${computerSelection} beats your ${playerSelection}. You lost that round.
-            Score is now You: ${playerScore} vs. Computer: ${computerScore}`; */
+            scoreMessage.textContent = `Computer's ${computerSelection} beats your ${playerSelection}. You lost that round.
+            Score is now You: ${playerScore} vs. Computer: ${computerScore}`; 
             console.log(`Computer's ${computerSelection} beats your ${playerSelection}. You lost that round.
             Score is now You: ${playerScore} vs. Computer: ${computerScore}`);
         } else {
             playerScore++;
-            /*results.textContent = `Your ${playerSelection} beats their ${computerSelection}. You win that round.
-            Score is now You: ${playerScore} vs. Computer: ${computerScore}`;*/
+            scoreMessage.textContent = `Your ${playerSelection} beats their ${computerSelection}. You win that round.
+            Score is now You: ${playerScore} vs. Computer: ${computerScore}`;
             console.log(`Your ${playerSelection} beats their ${computerSelection}. You win that round.
             Score is now You: ${playerScore} vs. Computer: ${computerScore}`);
         }
     }
-
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            playRound(playerSelection = `${button.id}`, computerSelection);
-        });
-    });
-
-
-
 };
 
 game();
